6928ec3ae2150344a4933ea3ed5dd46729cf56f1
rptra <rifqempul@gmail.com>
2021-12-19 23:44:14 +0700

    convert to md

--- src/lat_03_io/start.rs
+++ src/lat_03_io/start.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::fs;
+use std::io::{BufRead, BufReader};
 
 use crate::lat_03_io::write as write;
 pub trait Writes {
@@ -9,8 +10,12 @@ pub trait Writes {
 impl Writes for write::Article {
     fn write(&self) -> () {
 
-
-        fs::write("dist/foo", &self.body )
+        let content = format!("# {} 
+{}  
+Category : {}
+        ", self.title, self.body, self.category);
+                    
+        fs::write("dist/foo.md", &content)
         .expect("Unable to write file");
 
 
@@ -19,24 +24,66 @@ impl Writes for write::Article {
     }
 }
 
+
 pub fn execute() {
-    println!("---> File : ");
 
 
+    let mut title = String::new();
     let mut isi = String::new();
 
 
+    println!("Article Title");
+    print!(" >> ");
+    io::stdin().read_line(&mut title).unwrap();
+
+    println!(" ");
+
+    println!("Article Body : ");
+    print!(" >> ");
     io::stdin().read_line(&mut isi).unwrap();
 
 
-    let write = write::Article{
-        title: String::from("new article"),
+    let raw_categories = fs::File::open("categories.txt").expect("file not found");
+
+    let mut raw_categories = BufReader::new(raw_categories);
+
+    let c = &mut raw_categories;
+    print_categories(c);
+
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+
+    let category = assign_category(&mut raw_categories, &mut input.trim().parse::<usize>().unwrap());
+
+    let article = write::Article{
+        title: title,
         body: isi,
-        category: vec![],
+        category: category,
         date: 8,
     };
 
-    write.write();
+    article.write();
 
+
+}
+
+fn print_categories(c:&mut BufReader<fs::File>) {
+            for (num, item) in c.lines().enumerate() {
+
+                println!("{}. {}", num, item.unwrap());
+
+            }
+
+}
+
+fn assign_category(c: &mut BufReader<fs::File>, input: &usize) -> String {
+    for (num, item) in c.lines().enumerate() {
+        if num == *input {
+            return String::from(item.unwrap());
+        }
+    }
+
+    String::from("Uncategorized")
 
 }
---
+++ src/lat_04_loop/nilai.rs
@@ -0,0 +1,29 @@
+
+
+pub fn toItemFromAbsen(absen: u8) -> NilaiItem {
+
+    
+    
+
+
+
+    
+
+}
+
+struct Mahasiswa {
+    name: String,
+    npm: String,
+    absen: u8,
+}
+
+struct DaftarNilai {
+    name: String,    
+}
+
+
+struct NilaiItem {
+    name: String,
+    nilai: u16,
+}
+
