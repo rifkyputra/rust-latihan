559d6ec7df298c442625f70b83b13c3bc49a237a
rptra <rifqempul@gmail.com>
2022-12-07 09:48:39 +0700

    lat06

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use lat_05_db::db;
+use lat_05_sqlite::db;
 use try_01_hello::*;
 
+use lat_06_aocday6::day6;
+
 mod lat_03_io;
-mod lat_05_db;
+mod lat_05_sqlite;
+mod lat_06_aocday6;
 mod try_01_hello;
 // use ::pokemon::start as poke;
 
 fn main() {
     // hello::prompt();
-    db::prompt();
+    // db::prompt();
+    day6::day6();
 
-    lat_03_io::start::execute();
+    // lat_03_io::start::execute();
 
     // poke::start();
 }
